93822407c2862c73707f5fed506b905c12b45c8a
Ashish Neupane <aasesshenry@gmail.com>
2021-06-05 13:50:59 +0545

    add to cart button event activated on trending product

--- src/component/trendingProduct/TrendingProduct.js
+++ src/component/trendingProduct/TrendingProduct.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
+import { useSelector,useDispatch } from 'react-redux';
+import {cart} from '../../features/productSlice'
 import "./TrendingProduct.css"
 
 const TrendingProduct = () => {
     const products = useSelector((state)=> state.product.products )
+    const dispatch = useDispatch()
     //filter out trenond product 
     const trendProduct = products.filter((product)=>{
         return product.trending
     }) 
+
+    //add to cart button is clicked
+    
+    const addCart =(product) => {
+        // console.log(product.product)
+        dispatch(cart({
+            id:product.product.id,
+            title:product.product.title,
+            price:product.product.price,
+            image:product.product.image,
+            qty: 1
+        }))
+    }
 
     const display = trendProduct.slice(0,6).map((product)=>{
         return(
@@ -16,7 +30,7 @@ const TrendingProduct = () => {
                 <div className="card fproduct" >
                     <img className = "card-top-img" src={product.image} alt={product.id} />
                     <div className="middle">
-                        <button className="btn btn-sm btn-info">Add to Cart</button>
+                        <button className="btn btn-sm btn-info"  onClick={()=>addCart({product})}>Add to Cart</button>
                     </div>
                     <div className="card-body">
                         <p className="card-text" style={{fontSize:"0.9rem"}}>{product.title}<span className="float-right">${product.price}</span></p>
